892c6a262fc1e72cc8d6bb2ff3e52b07243db3a8
Megax-yt <67208750+Megax-yt@users.noreply.github.com>
2023-07-22 02:18:34 -0400

    Update xloleo.py

--- xloleo.py
+++ xloleo.py
@@ -40,9 +40,7 @@ def draw_boxes(frame, boxes):
         x1, y1, x2, y2, conf, cls = box
         label = f"{conf * 100:.2f}%"
         color = (0, 255, 0)  # Green
-        #cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
-        #cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
+        
         # Convert bounding box coordinates to YOLO format
         img_height, img_width, _ = frame.shape
         x_center = (x1 + x2) / (2 * img_width)
@@ -66,7 +64,7 @@ def main():
     detector = ObjectDetector(model_path, display_threshold)
     clear()
 
-    lastd = None  # Initialize lastd variable
+    lastd = None
 
     with mss.mss() as sct:
         monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}  
